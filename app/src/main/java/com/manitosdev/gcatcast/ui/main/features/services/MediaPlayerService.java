b432998d0508a4d3e327c535360fd5342167cb82
Gilberto Hernandez G <ghernandez.9002@gmail.com>
2020-06-13 00:24:17 -0500

    feat: config the MediaPlayer

--- app/src/main/java/com/manitosdev/gcatcast/ui/main/features/services/MediaPlayerService.java
+++ app/src/main/java/com/manitosdev/gcatcast/ui/main/features/services/MediaPlayerService.java
@@ -6,7 +6,9 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 import androidx.annotation.Nullable;
+import java.io.IOException;
 
 /**
  * Created by gilbertohdz on 13/06/20.
@@ -24,6 +26,15 @@ public class MediaPlayerService extends Service
   // Binder given to clients
   private final IBinder iBinder = new LocalBinder();
 
+  /**
+   * 2.- Config media player
+   */
+  private MediaPlayer mediaPlayer;
+  // path to the audio file
+  private String mediaFile;
+  // Used to pause/resume MediaPlayer
+  private int resumePosition;
+
   @Nullable
   @Override
   public IBinder onBind(Intent intent) {
@@ -33,6 +44,9 @@ public class MediaPlayerService extends Service
   @Override
   public void onCompletion(MediaPlayer mp) {
     // Invoked when playback of a media source has completed.
+    stopMedia();
+    // stop the service
+    stopSelf();
   }
 
   @Override
@@ -49,6 +63,17 @@ public class MediaPlayerService extends Service
   @Override
   public boolean onError(MediaPlayer mp, int what, int extra) {
     // Invoked when there has been an error during an asynchronous operation.
+    switch (what) {
+      case MediaPlayer.MEDIA_ERROR_NOT_VALID_FOR_PROGRESSIVE_PLAYBACK:
+        Log.d("MediaPlayer Error", "MEDIA ERROR NOT VALID FOR PROGRESSIVE PLAYBACK: " + extra);
+        break;
+      case MediaPlayer.MEDIA_ERROR_SERVER_DIED:
+        Log.d("MediaPlayer Error", "MEDIA ERROR SERVER DIED: " + extra);
+        break;
+      case MediaPlayer.MEDIA_ERROR_UNKNOWN:
+        Log.d("MediaPlayer Error", "MEDIA ERROR UNKNOWN: " + extra);
+        break;
+    }
     return false;
   }
 
@@ -61,11 +86,62 @@ public class MediaPlayerService extends Service
   @Override
   public void onPrepared(MediaPlayer mp) {
     // Invoked when the media source is ready for playback.
+    playMedia();
   }
 
   @Override
   public void onSeekComplete(MediaPlayer mp) {
     // Invoked indicating the completion of a seek operation.
+  }
+
+  private void initMediaPlayer() {
+    mediaPlayer = new MediaPlayer();
+    // Set up MediaPlayer event listeners
+    mediaPlayer.setOnCompletionListener(this);
+    mediaPlayer.setOnErrorListener(this);
+    mediaPlayer.setOnPreparedListener(this);
+    mediaPlayer.setOnBufferingUpdateListener(this);
+    mediaPlayer.setOnSeekCompleteListener(this);
+    mediaPlayer.setOnInfoListener(this);
+    // Reset so that the MediaPlayer is not pointing to another data source
+    mediaPlayer.reset();
+
+    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    try {
+      // Set the data source to the mediaFile location
+      mediaPlayer.setDataSource(mediaFile);
+    } catch (IOException e) {
+      e.printStackTrace();
+      stopSelf();
+    }
+    mediaPlayer.prepareAsync();
+  }
+
+  private void playMedia() {
+    if (!mediaPlayer.isPlaying()) {
+      mediaPlayer.start();
+    }
+  }
+
+  private void stopMedia() {
+    if (mediaPlayer == null) return;
+    if (mediaPlayer.isPlaying()) {
+      mediaPlayer.stop();
+    }
+  }
+
+  private void pauseMedia() {
+    if (mediaPlayer.isPlaying()) {
+      mediaPlayer.pause();
+      resumePosition = mediaPlayer.getCurrentPosition();
+    }
+  }
+
+  private void resumeMedia() {
+    if (!mediaPlayer.isPlaying()) {
+      mediaPlayer.seekTo(resumePosition);
+      mediaPlayer.start();
+    }
   }
 
   public class LocalBinder extends Binder {
